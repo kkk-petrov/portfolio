0e6d83d550e4f202602419db8e8bd518ed70cbf6
kkk-petrov <kkpetrov.dev@gmail.com>
2022-04-23 16:53:56 +0300

    init: initialize project, feat: made a header

---
+++ src/components/Skill.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Skill.css';
+
+function Skill({items}){
+    return(
+        items.map((item) => (
+            <li className="skills__item skill">
+                <img src={item.icon} alt="html" className="skill__icon" />
+                <p className="skill__title">{item.name}</p>
+            </li>
+        ))
+
+        
+    )
+}
+
+export default Skill;
